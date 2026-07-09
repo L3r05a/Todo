409ee9407d880882d8ce8063972cd367b9a5b822
l3r05a <200935142+L3r05a@users.noreply.github.com>
2026-07-09 16:30:03 +0100

    Complete Todo Project

--- webpack.config.js
+++ webpack.config.js
@@ -3,15 +3,17 @@ import path from "node:path";
 import HtmlWebpackPlugin from "html-webpack-plugin";
 
 export default {
-    mode: "development",
+    
     entry: "./src/index.js",
     output: {
         filename: "main.js",
         path: path.resolve(import.meta.dirname, "dist"),
         clean: true,
+        publicPath: "/Todo/",
     },
 
     devtool: "eval-source-map",
+    
     devServer: {
         watchFiles: ["./src/template.html"],
     },
